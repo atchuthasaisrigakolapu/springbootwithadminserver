30a82c6fbd8b046b4a07a68630a833351952df4a
atchuthasaisrigakolapu <atchuthasaisrigakolapu@gmail.com>
2022-05-22 15:04:50 +0530

    initial commit

---
+++ src/main/java/com/example/springbootwithadminserver/SpringbootwithadminserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootwithadminserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwithadminserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootwithadminserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootwithadminserver/SpringbootwithadminserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootwithadminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootwithadminserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
